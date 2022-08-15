a7f3ea7fc3116c6ba5a38b55705955b4e2995e26
hillinhank <101481759+hillinhank@users.noreply.github.com>
2022-08-15 06:58:15 -0500

    Add files via upload

---
+++ Dashboard/app.py
@@ -0,0 +1,39 @@
+#import dependencies
+from flask import Flask, render_template, redirect, url_for
+from flask_pymongo import PyMongo
+# import scraping
+import random
+# import requests
+from config import MONGO_URL
+import os
+
+
+#set up flask
+app = Flask(__name__)
+
+#Use pyMondo to set up mongo connection
+app.config["MONGO_URL"] = os.environ.get(MONGO_URL)
+mongo = PyMongo(app)
+
+
+@app.route('/')
+def home():
+    return render_template('index2.html')
+
+@app.route('/results')
+def results():
+    return render_template('results.html')
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
+
+
+
+
+
+
+
+
+
+
+
